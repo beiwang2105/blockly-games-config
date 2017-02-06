b807398ba893f984cdfdacde950626ae5755ac63
Leon <leon.zcf@gmail.com>
2017-02-06 18:07:27 +0800

    update

--- local/common/interface.js
+++ local/common/interface.js
@@ -1,25 +1,43 @@
+var scriptLevelID = null;
+
 $(function(){
-  
-  $('#runButton').bind('click', function(){
-    var scriptLevelId = 0;
-    window.location.search.split(/[&\?]+/).forEach(function(str){
-      var kAndV = str.split('=');
-      if(kAndV[0] == 'script_level_id'){scriptLevelId = kAndV[1];}
-    });
-    
-    $.ajax({
-      url: 'http://studio.istemedu.com/api/v1/script_levels/update_user_level',
-      method: 'POST',
-      data: {
-        id: scriptLevelId,
-        data: BlocklyInterface.getCode()
-      },
-      xhrFields: {
-        withCredentials: true
-      }
-    }).done(function(data){
-      console.log(data);
-    });
-  });
+
+  initAnswer();
   
 });
+
+$('#runButton').bind('click', function(){
+  $.ajax({
+    url: 'http://studio.istemedu.com/api/v1/script_levels/update_user_level',
+    method: 'POST',
+    data: {
+      id: scriptLevelId(),
+      data: BlocklyInterface.getCode()
+    },
+    xhrFields: {
+      withCredentials: true
+    }
+  }).done(function(data){
+    console.log(data);
+  });
+});
+
+function initAnswer() {
+  $.ajax({
+    url: 'http://studio.istemedu.com/api/v1/script_levels/'+scriptLevelId(),
+    method: 'GET',
+    xhrFields: {
+      withCredentials: true
+    }
+  }).done(function(data){
+    console.log(data);
+  });
+};
+
+function scriptLevelId(){
+  window.location.search.split(/[&\?]+/).forEach(function(str){
+    var kAndV = str.split('=');
+    if(kAndV[0] == 'script_level_id'){scriptLevelID = kAndV[1];}
+  });
+  return scriptLevelID;
+};
