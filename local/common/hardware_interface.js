a98b96597400638cf8584f0782b1b25407271b7c
Leon <leon.zcf@gmail.com>
2017-02-10 11:38:20 +0800

    fix

--- local/common/hardware_interface.js
+++ local/common/hardware_interface.js
@@ -1,4 +1,21 @@
-$('#runButton').bind('click', function(){
+function initAnswer(){
+  $.ajax({
+    url: 'http://studio.istemedu.com/api/v1/script_levels/get_answer',
+    method: 'GET',
+    data: {
+      script: Blockly4Pi.Script,
+      stage: Blockly4Pi.Stage,
+      level: BlocklyGames.LEVEL
+    },
+    xhrFields: {
+      withCredentials: true
+    }
+  }).done(function(data){
+    console.log(data);
+  });
+};
+
+function saveAnswer(){
   $.ajax({
     url: 'http://studio.istemedu.com/api/v1/script_levels/answer',
     method: 'POST',
@@ -7,23 +24,6 @@ $('#runButton').bind('click', function(){
       stage: Blockly4Pi.Stage,
       level: BlocklyGames.LEVEL,
       data: BlocklyInterface.getCode()
-    },
-    xhrFields: {
-      withCredentials: true
-    }
-  }).done(function(data){
-    console.log(data);
-  });
-});
-
-function initAnswer() {
-  $.ajax({
-    url: 'http://studio.istemedu.com/api/v1/script_levels/get_answer',
-    method: 'GET',
-    data: {
-      script: Blockly4Pi.Script,
-      stage: Blockly4Pi.Stage,
-      level: BlocklyGames.LEVEL
     },
     xhrFields: {
       withCredentials: true
